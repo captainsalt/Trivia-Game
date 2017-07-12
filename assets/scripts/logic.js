47db90e87dda454965c33839ffbe91e8617f8f64
David Wright <dontbothertheking@gmail.com>
2017-07-11 19:27:13 -0500

    chnaged populateQuizto updateQuiz

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -3,36 +3,33 @@
     var choiceElements = $("[id^=choice]").get();
     var selectedElement = null;
     var defaultColor = "white";
-    var selectedColor = "black";
+    var selectedColor = "gray";
 
-    populateQuiz();
+    resetColors();
+    updateQuiz();
 
     $(choiceElements).click(e => {
         resetColors();
         selectedElement = $(e.currentTarget).css("background", selectedColor);
     });
 
-    function resetColors() {
-        $(choiceElements).css("background", defaultColor);
-    }
-
-    //next button handler
+    //next button click handler
     $("#nextButton").click(() => {
+        //If no answer is selected
         if (selectedElement === null) {
             alert("Please select an answer");
             return;
         }
 
         game.nextQuestion();
-        //relect the change to questions left
         $("#questionsLeft").html(game.questionsLeft);
-        populateQuiz();
+        updateQuiz();
         resetColors();
         selectedElement = null;
     });
-
-    //populates the quiz html
-    function populateQuiz() {
+    
+    //updates the quiz html
+    function updateQuiz() {
         //populate question element
         var question = $("#question");
         question.html(game.question);
@@ -49,6 +46,10 @@
             element.html(game.answers[answerChoiceIndex++]);
         }
     }
+
+    function resetColors() {
+        $(choiceElements).css("background", defaultColor);
+    }
 });
 
 //pass in quiz 1, 2 etc
@@ -58,7 +59,7 @@ function Game(quiz) {
     this.question = this.quiz[this.index].question;
     this.answers = this.quiz[this.index].answers;
     this.correctAnswer = this.quiz[this.index].correctAnswer;
-    this.questionsLeft = this.quiz.length;
+    this.questionsLeft = --this.quiz.length;
 
     this.nextQuestion = () => {
         this.index++;
@@ -81,35 +82,30 @@ var timer = {
 
 var quizJson = {
     "quiz1": [{
-            "questionNumber": 1,
             "question": "What is 1 + 1?",
             "answers": ["2", "4", "3", "1"],
             "correctAnswer": "2"
         },
         {
-            "questionNumber": 2,
             "question": "What is 2 + 2?",
             "answers": ["1", "2", "8", "4"],
             "correctAnswer": "4"
         },
         {
-            "questionNumber": 3,
             "question": "What is 7 * 3?",
             "answers": ["10", "21", "30", "17"],
             "correctAnswer": "21"
         },
         {
-            "questionNumber": 4,
             "question": "What is 6/2(1+2)?",
             "answers": ["1", "2", "9", "8"],
             "correctAnswer": "9"
         },
         {
-            "questionNumber": 5,
             "question": "What is 10 * 2(5 * (5 + 1))?",
             "answers": ["520", "600", "521", "530"],
             "correctAnswer": "600"
-        }
+        },
     ],
 
     "quiz2": [{
